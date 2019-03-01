aa0a22229c3cff640c8f79d74e77d0252b7b6c5b
Michael Gearhardt <michael.gearhardt@fathym.com>
2019-02-28 17:51:48 -0700

    reference cleanup and prep for testing app manageent LCU

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ContentElementContext } from 'projects/common/src/lcu.api';
 
 @Component({
   selector: 'lcu-root',
@@ -8,30 +7,12 @@ import { ContentElementContext } from 'projects/common/src/lcu.api';
 })
 export class AppComponent implements OnInit {
   //  Properties
-  public Contexts: ContentElementContext[];
 
   //  Constructors
   constructor() {
-    this.Contexts = [];
   }
 
   //  Life Cycle
   public ngOnInit() {
-    this.Contexts.push({
-      State: {
-        Content: 'Hello World',
-        Type: 'h1',
-        Class: 'this-world'
-      }
-    });
-
-    this.Contexts.push({
-      State: {
-        Content: 'http://fathym.com',
-        Type: 'iframe',
-        Class: 'that-world',
-        Modifier: `frameborder="0"`
-      }
-    });
   }
 }
